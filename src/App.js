2c22313e7a02ac210599ebe9299d072d2fb93dcc
Dani-jardim <danialvesdesigner@gmail.com>
2020-08-17 17:46:41 -0300

    criando label de eventos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-
 import React from 'react';
 import './App.css';
+import Label from './objects/Label';
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
+
 
 const App = () => (
   <main className="app">
     <Header />
     <Hashtag />
+    <Label content="Mostrar Eventos" />
   </main>
   )
 
