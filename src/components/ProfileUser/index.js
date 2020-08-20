0986690966a1f7337f27e135bc3f0cbb7ec54942
Dani-jardim <danialvesdesigner@gmail.com>
2020-08-20 20:10:49 -0300

    adicionando respiro ao AvatarProfile

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -7,9 +7,8 @@ import AvatarProfile from '../../objects/AvatarProfile';
 
 const ProfileUser = () => (
   <dl className="profile-user">
-
     <dd className="avatar">
-      <AvatarProfile src={Profile} alt= "Avatar Daniela" />
+      <AvatarProfile className="avatar-profile" src={Profile} alt= "Avatar Daniela" />
     </dd>
     <dt className="title">Dani Jardim</dt>
     <dd className="description">Sou designer gráfica e frontend em início de carreira. Moro em São Paulo. As minhas stacks atuais são: Html, CSS, JavaScript e React
